13731ee7cb49b31cd47baa3057de32e2ab974600
ravindragsingh <arien.641@gmail.com>
2018-06-07 21:15:56 -0400

    login page

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+//import Header from './Header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Blockchain Based Ration Distribution System</h1>
+        </header>
+        <p className="App-intro">
+        </p>
+
+<div><Login/>
+</div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+       <p><input
+                  type = "text"
+                  placeholder = "Enter the username"
+                  />
+        </p>
+         <p>
+         <input
+                         type = "text"
+                         placeholder = "Enter the password"
+                     />
+
+         </p>
+
+          <p>
+          <select>
+                     <option value="none" disabled>Select category</option>
+                     <option value="farmer">Farmer</option>
+                     <option value="procurement">Procurement</option>
+                     <option value="storage">Storage</option>
+                     <option value="distributor">Distributor</option>
+                     <option value="fps">Fair price shop</option>
+                     <option value="consumer">Consumer</option>
+                                </select>
+
+
+          </p>
+
+        <div > <button class = "button">Login </button></div>
+
+
+      </div>
+    )
+  }
+}
+
+export default Login
