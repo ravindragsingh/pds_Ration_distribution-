27086cbaadc12ee01fed43d6554a3a109a2cb9be
ravindragsingh <arien.641@gmail.com>
2018-06-14 23:13:59 -0400

    name updated

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Blockchain Based Ration Distribution System</h1>
+          <h1 className="App-title">Blockchain Based Ration Distribution System1</h1>
         </header>
         <p className="App-intro">
         </p>
