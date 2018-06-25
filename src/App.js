969835b271f309c115c397b9829e76ef90a25aca
ravindragsingh <arien.641@gmail.com>
2018-06-25 01:59:14 -0400

    Farmers dummy page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
-import loginUI from './loginUI';
 import howitworks from './images/PDS1.PNG';
 import { Route } from 'react-router-dom';
 import Farmers from './components/Farmers';
---
+++ src/components/Farmers.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import { Route } from 'react-router-dom'
+
+class Farmers extends Component {
+  render() {
+    return (
+    <div>
+    <h2>You are logged in as Farmer</h2>
+
+    </div>
+
+      )
+  }
+}
+
+export default Farmers;
