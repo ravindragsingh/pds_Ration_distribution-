6c7a902458a11ea46748cb0ebe86556754d40364
ravindragsingh <arien.641@gmail.com>
2018-06-25 01:55:08 -0400

    Farmers page added

--- src/App.js
+++ src/App.js
@@ -4,33 +4,48 @@ import './App.css';
 import Login from './Login';
 import loginUI from './loginUI';
 import howitworks from './images/PDS1.PNG';
-//import Header from './Header'
-import {BrowserRouter} from 'react-router-dom'
+import { Route } from 'react-router-dom';
+import Farmers from './components/Farmers';
 
 class App extends Component {
   render() {
-
     return (
-<BrowserRouter>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Blockchain Based Ration Distribution System1</h1>
+          <h1 className="App-title">Blockchain Based Ration Distribution System</h1>
         </header>
         <p className="App-intro">
         </p>
+    <Route path = "/login"
+    render = {
+    () => {
+           return(
+            <div>
+                        <Login/>
+            </div>
+           )
+    }
+    }
+    />
+     <Route path = "/farmers"
+        render = {
+        () => {
+               return(
+                <div>
+                            <Farmers/>
+                </div>
+               )
+        }
+        }
+        />
 
-<div><Login/>
-</div>
     <div >
     <li>
     </li>
        <img src ={howitworks}/>
     </div>
       </div>
-
-</BrowserRouter>
-
 
     );
   }
