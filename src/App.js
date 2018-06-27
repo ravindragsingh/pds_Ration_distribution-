f5117cdd36edf5ec3a55f662665ac7677c2d6dde
ravindragsingh <arien.641@gmail.com>
2018-06-27 17:00:18 -0400

    appBar adde

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './Login';
 import howitworks from './images/PDS1.PNG';
 import { Route } from 'react-router-dom';
 import Farmers from './components/Farmers';
+import AppBar from './components/AppBar';
 
 class App extends Component {
   render() {
@@ -12,15 +13,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Blockchain Based Ration Distribution System</h1>
+          <h1 className="App-title"></h1>
         </header>
         <p className="App-intro">
         </p>
-    <Route path = "/login"
+    <Route path = "/"
     render = {
     () => {
            return(
             <div>
+            <AppBar text = 'Blockchain Based Ration Distribution System'/>
                         <Login/>
             </div>
            )
